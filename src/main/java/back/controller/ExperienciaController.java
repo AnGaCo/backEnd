88fe0641ecc2664abde70fa150592c8d66f35d59
Manuel Orozco <oroman6677@gmail.com>
2023-01-30 21:59:35 -0300

    Se modificaron nuevamente los controladores

--- src/main/java/back/controller/ExperienciaController.java
+++ src/main/java/back/controller/ExperienciaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/experiencia")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ExperienciaController {
     
     @Autowired
--- src/main/java/back/controller/TecnologiaController.java
+++ src/main/java/back/controller/TecnologiaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tecnologia")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TecnologiaController {
     
     @Autowired
