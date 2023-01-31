b17f4f91cc06845e23008d30149fa083833f1790
Manuel Orozco <oroman6677@gmail.com>
2023-01-30 21:41:30 -0300

    Se agrego CrossOrigin en los controladores

--- src/main/java/back/controller/ProyectoController.java
+++ src/main/java/back/controller/ProyectoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/proyecto")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://localhost:4200")
 public class ProyectoController {
     
     @Autowired
